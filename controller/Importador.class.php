f8725fff829500ba6adbf6d460e5ea1fd987f48c
= <thiagorthomaz@gmail.com>
2016-07-12 23:23:07 -0300

    Adiconado arquivos para importação dos proventos

--- controller/Importador.class.php
+++ controller/Importador.class.php
@@ -8,112 +8,192 @@ namespace app\controller;
  * @author thiago
  */
 class Importador extends \stphp\Controller {
-  
-  public function importarBase(){
-    
-    ini_set('memory_limit', '1024M'); 
-    
-    
+
+  public function importarBase() {
+
+    ini_set('memory_limit', '1024M');
+
+
     $dao = new \app\model\PrecoDAO();
-    
+
     //$anos = array('2009','2010','2011','2012','2013','2014','2015');
     $anos = array('2016');
-    
-    foreach ($anos as $ano){
+
+    foreach ($anos as $ano) {
       echo "\nIniciado a importação do ano de " . $ano;
       $filename = "COTAHIST_A" . $ano . ".TXT";
       $imp_cotacoes = new \app\model\ImportadorCotacoes($dao, $ano);
       $imp_cotacoes->setPath("/var/www/html/simulador-acoes/cotahist/");
-      $imp_cotacoes->importaArquivo($filename);  
+      $imp_cotacoes->importaArquivo($filename);
       echo "\nFinalizado a importação do ano de " . $ano;
     }
-    
+
     echo "-------------- CONCLUÍDO! -------------------";
     exit;
   }
+
   
-  public function importarProventos(){
-    
-    
-    
-    
+  
+  //http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_D11072016.ZIP
+  public function importadorDiario() {
+
+    echo date("dmY");
+
+
+    exit;
+  }
+  
+  public function importarProventosBase(){
     $preco_dao = new \app\model\PrecoDAO();
     $lista_ativos = $preco_dao->listarAtivos();
 
+    $caminho = "/var/www/html/simulador-acoes/cotahist/proventos/";
+    $caminho_arquivo = "/var/www/html/simulador-acoes/cotahist/hist_proventos.sql";
+
+    $handle_hist = fopen($caminho_arquivo, "w+");
+    
+    foreach ($lista_ativos as $ativo) {
+      $cod_ativo = $ativo["cod_ativo"];
+      $caminho_completo = $caminho . $cod_ativo . ".html";
+      
+      
+      $handle = fopen($caminho_completo, "r");
+      if ($handle) {
+        $body = false;
+        while (($buffer = fgets($handle, 4096)) !== false) {
+          $linha = trim(fgets($handle));
+          if (strpos($linha, "<tbody>") !== false) {
+            $body = true;
+            continue;
+          }
+          
+          if ($body) {
+            $slipt = explode("<td>", $linha);
+            unset($slipt[0]);
+            if (count($slipt) === 4) {
+              $tipo = str_replace("</td>", "", $slipt[1]);
+              $data = str_replace("</td>", "", $slipt[2]);
+              $descricao = str_replace("</td>", "", $slipt[3]);
+              
+              $sql = "insert into Tab_hist_provento (cod_ativo, tipo, data, descricao) values ('" . $cod_ativo . "', '". $tipo . "', '". $data . "', '" . $descricao . "');";
+              fwrite($handle_hist, utf8_encode($sql) . "\n");
+              
+            }
+
+          }
+          
+          if (strpos($linha, "</tbody>") !== false){
+            $body = false;
+            continue;
+          }
+
+        }
+        
+      }
+      
+      fclose($handle);
+      
+    }
+    
+    fclose($handle_hist);
+    echo "Finalizado!";
+    
+    exit;
+    
+  }
+
+
+  
+  public function importarProventos() {
+
+    $preco_dao = new \app\model\PrecoDAO();
+    $lista_ativos = $preco_dao->listarAtivos();
+    
     foreach ($lista_ativos as $ativo) {
       $cod_ativo = $ativo["cod_ativo"];
 
       $destino = "/var/www/html/simulador-acoes/cotahist/proventos/" . $cod_ativo . ".html";
       $url_fonte = "http://www.guiainvest.com.br/provento/default.aspx?sigla=" . $cod_ativo;
-      
+      if (file_exists($destino)) {
+        continue;
+      }
+
       $handle_importacao = fopen($destino, "w+");
-      if (!$handle_importacao){
+      if (!$handle_importacao) {
         echo "Erro ao criar o arquivo de destino";
         exit;
       }
 
       $handle_fonte = @fopen($url_fonte, "r");
       if ($handle_fonte) {
-          $gravando = false;
-          while (($buffer = fgets($handle_fonte, 4096)) !== false) {
-            if (trim($buffer) == '<div id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao" class="RadGrid RadGrid_Padrao">'){
-              $gravando = true;
-            }
-            if ($gravando){
-              $buffer = str_replace('<table cellspacing="0" class="rgMasterTable" border="0" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00" style="width:100%;table-layout:auto;empty-cells:show;">', '<table>', $buffer);
-              $buffer = str_replace('<colgroup>', '', $buffer);
-              $buffer = str_replace('<col  />', '', $buffer);
-              $buffer = str_replace('</colgroup>', '', $buffer);
+        $gravando = false;
+        while (($buffer = fgets($handle_fonte, 4096)) !== false) {
+          if (trim($buffer) == '<div id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao" class="RadGrid RadGrid_Padrao">') {
+            $gravando = true;
+          }
+          if ($gravando) {
+            $buffer = str_replace('<table cellspacing="0" class="rgMasterTable" border="0" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00" style="width:100%;table-layout:auto;empty-cells:show;">', '<table>', $buffer);
+            $buffer = str_replace('<colgroup>', '', $buffer);
+            $buffer = str_replace('<col  />', '', $buffer);
+            $buffer = str_replace('</colgroup>', '', $buffer);
 
-              $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__0">', '<tr>', $buffer);
-              $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__1">', '<tr>', $buffer);
-              $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__2">', '<tr>', $buffer);
-              $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__3">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__0">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__1">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__2">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__3">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__4">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__5">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__6">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__7">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__8">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__9">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__10">', '<tr>', $buffer);
+            
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__1">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__2">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__3">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__4">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__5">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__6">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__7">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__8">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__9">', '<tr>', $buffer);
+            $buffer = str_replace('<tr class="rgAltRow" id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ctl00__10">', '<tr>', $buffer);
+            
+            
 
-              
-              $buffer = str_replace('<th scope="col" class="rgHeader" style="white-space:nowrap;text-align:center;">', '<th>', $buffer);
-              $buffer = str_replace('<th scope="col" class="rgHeader">', '<th>', $buffer);
-              $buffer = str_replace('<th scope="col" class="rgHeader tdMobile320" style="text-align:center;">', '<th>', $buffer);
-              $buffer = str_replace('<td align="center" style="white-space:nowrap;">', '<td>', $buffer);
-              $buffer = str_replace('<td class="tdMobile320" align="center" style="white-space:nowrap;">', '<td>', $buffer);
-                            
-              $buffer = str_replace('<input id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ClientState" name="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ClientState" type="hidden" />', '', $buffer);
-              
-              fwrite($handle_importacao, $buffer);  
-            }
 
+            $buffer = str_replace('<th scope="col" class="rgHeader" style="white-space:nowrap;text-align:center;">', '<th>', $buffer);
+            $buffer = str_replace('<th scope="col" class="rgHeader">', '<th>', $buffer);
+            $buffer = str_replace('<th scope="col" class="rgHeader tdMobile320" style="text-align:center;">', '<th>', $buffer);
+            $buffer = str_replace('<td align="center" style="white-space:nowrap;">', '<td>', $buffer);
+            $buffer = str_replace('<td class="tdMobile320" align="center" style="white-space:nowrap;">', '<td>', $buffer);
 
-            if (trim($buffer) == '</div>' && $gravando){
-              $gravando = false;
-            }
+            $buffer = str_replace('<input id="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ClientState" name="ctl00_ctl00_cphConteudo_cphConteudo_ProventoAcao1_RadGridProventoAcao_ClientState" type="hidden" />', '', $buffer);
 
+            fwrite($handle_importacao, $buffer);
           }
 
-          if (!feof($handle_fonte)) {
-              echo "Error: unexpected fgets() fail\n";
+
+          if (trim($buffer) == '</div>' && $gravando) {
+            $gravando = false;
           }
-          fclose($handle_importacao);
-          fclose($handle_fonte);
+        }
+
+        if (!feof($handle_fonte)) {
+          echo "Error: unexpected fgets() fail\n";
+        }
+        fclose($handle_importacao);
+        fclose($handle_fonte);
       }
     }
-    
-    
-    
-    
+
+
+
+
     echo "Importação concluída!";
-    
+
     exit;
   }
-  
-  
-  //http://bvmf.bmfbovespa.com.br/InstDados/SerHist/COTAHIST_D11072016.ZIP
-  public function importadorDiario(){
-    
-    echo date("dmY");
-    
-    
-    exit;
-    
-  }
-  
+
 }
--- model/PrecoDAO.class.php
+++ model/PrecoDAO.class.php
@@ -32,7 +32,8 @@ class PrecoDAO extends \app\model\DAO {
   }
   
   public function listarAtivos(){
-    $sql = "select distinct cod_ativo from Tab_preco where negocios > 25000 order by 1";
+    //$sql = "select distinct cod_ativo from Tab_preco where negocios > 25000 order by 1";
+    $sql = "select distinct cod_ativo from Tab_preco";
     $rs = $this->sendQuery($sql);
     return $rs->getResultSet();
   }
